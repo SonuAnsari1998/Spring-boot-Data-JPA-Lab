bb589daf258e495eb29508e084ab57f4dbcc9be9
Sonu Ansari <786sonuansari123@gmail.com>
2026-04-10 18:10:39 +0530

    Task 2: Order Management System

--- april_06_2026_Data_JPA_02_Finder_Method_Order_Management_System/src/main/java/com/nit/runner/OrderRunner.java
+++ april_06_2026_Data_JPA_02_Finder_Method_Order_Management_System/src/main/java/com/nit/runner/OrderRunner.java
@@ -1,5 +1,7 @@
 package com.nit.runner;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,43 +31,67 @@ public class OrderRunner implements CommandLineRunner {
 		System.out.println("10. Find by Status In");
 		System.out.println("0. Exit");
 		int choice = Integer.parseInt(IO.readln("Enter Your choice"));
-		switch(choice) {
-		case 1->{
+		switch (choice) {
+		case 1 -> {
 			String customerName = IO.readln("Enter Customer name want to find");
-				List<Order> byCustomerName = orderService.findByCustomerName(customerName);
-				byCustomerName.forEach(IO::println);
+			List<Order> byCustomerName = orderService.findByCustomerName(customerName);
+			byCustomerName.forEach(IO::println);
 		}
-		case 2->{
-			
+		case 2 -> {
+			String status = IO.readln("Enter status");
+			List<Order> byStatus = orderService.findByStatus(status);
+			byStatus.forEach(IO::println);
 		}
-		case 3->{
-			
+		case 3 -> {
+			double amt = Double.parseDouble(IO.readln("Enter Amount"));
+			List<Order> byAmountGreaterThan = orderService.findByAmountGreaterThan(amt);
+			byAmountGreaterThan.forEach(IO::println);
 		}
-		case 4->{
-			
+		case 4 -> {
+			double min = Double.parseDouble(IO.readln("Enter Min Amount"));
+			double max = Double.parseDouble(IO.readln("Enter Max Amount"));
+			List<Order> byAmountBetween = orderService.findByAmountBetween(min, max);
+			byAmountBetween.forEach(IO::println);
 		}
-		case 5->{
-			
+		case 5 -> {
+			LocalDate date = LocalDate.parse(IO.readln("Enter Date (YYYY-MM-DD)"));
+			List<Order> byOrderDateAfter = orderService.findByOrderDateAfter(date);
+			byOrderDateAfter.forEach(IO::println);
 		}
-		case 6->{
-			
+		case 6 -> {
+			LocalDate startDate = LocalDate.parse(IO.readln("Enter Start Date (YYYY-MM-DD)"));
+			LocalDate endDate = LocalDate.parse(IO.readln("Enter End Date (YYYY-MM-DD)"));
+			List<Order> byOrderDateBetween = orderService.findByOrderDateBetween(startDate, endDate);
+			byOrderDateBetween.forEach(IO::println);
 		}
-		case 7->{
-			
+		case 7 -> {
+			String status = IO.readln("Enter status");
+			double amt = Double.parseDouble(IO.readln("Enter Amount"));
+			List<Order> byStatusAndAmountGreaterThan = orderService.findByStatusAndAmountGreaterThan(status, amt);
+			byStatusAndAmountGreaterThan.forEach(IO::println);
 		}
-		case 8->{
-			
+		case 8 -> {
+			String key = IO.readln("Enter keyword");
+			List<Order> byProductNameContaining = orderService.findByProductNameContaining(key);
+			byProductNameContaining.forEach(IO::println);
 		}
-		case 9->{
-			
+		case 9 -> {
+			List<Order> top5ByOrderByOrderDateDesc = orderService.findTop5ByOrderByOrderDateDesc();
+			top5ByOrderByOrderDateDesc.forEach(IO::println);
 		}
-		case 10->{
-			
+		case 10 -> {
+			List<String> list = new ArrayList<String>();
+			int ch = Integer.parseInt(IO.readln("Enter how many status search"));
+			for (int i = 1; i <= ch; i++) {
+				String st = IO.readln("Enter status " + i);
+				list.add(st);
+			}
 		}
-		case 0->{
-			
+		case 0 -> {
+			System.out.println("Thank You!!!");
+			System.exit(0);
 		}
-		
+
 		default -> System.err.println("Invalid choice");
 		}
 	}
